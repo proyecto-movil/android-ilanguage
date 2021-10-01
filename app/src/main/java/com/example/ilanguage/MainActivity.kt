066b3c2fa3bed7e71e04a0819a7b9b26107d6d1c
XantX <hp_xantx@hotmail.com>
2021-09-30 22:54:52 -0500

    test

--- app/src/main/java/com/example/ilanguage/MainActivity.kt
+++ app/src/main/java/com/example/ilanguage/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.ilanguage
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun changeActivity(view: View){
+       val intent = Intent(this,MainMenuActivity::class.java)
+       startActivity(intent)
     }
 }
