3d2ba0f48c75b8eb73516757759f8de0ed621036
XantX <hp_xantx@hotmail.com>
2021-10-01 00:06:06 -0500

    feature: add function to change activity description: add funciton to change to profile activity to main menu TODO: change de main activity to launche

--- app/src/main/java/com/example/ilanguage/MainMenuActivity.kt
+++ app/src/main/java/com/example/ilanguage/MainMenuActivity.kt
@@ -1,9 +1,11 @@
 package com.example.ilanguage
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.View
 
 class MainMenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +17,9 @@ class MainMenuActivity : AppCompatActivity() {
         val inflater: MenuInflater = menuInflater
         inflater.inflate(R.menu.profile_menu,menu)
         return true
+    }
+    fun GoToProfile(view: View){
+        val intent = Intent(this, ProfileViewActivity::class.java)
+        startActivity(intent)
     }
 }
