fa603547a67ccbd0e6d1bb7d6abf0babcd71e8cc
XantX <hp_xantx@hotmail.com>
2021-10-01 07:59:39 -0500

    feature: add card to main menu description: add card to main menu about the session

--- app/src/main/java/com/example/ilanguage/editprofile.kt
+++ app/src/main/java/com/example/ilanguage/editprofile.kt
@@ -1,11 +1,25 @@
 package com.example.ilanguage
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class editprofile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_editprofile)
+        val btSave = findViewById<Button>(R.id.btsave)
+        val btCancel = findViewById<Button>(R.id.btcancel)
+        btSave.setOnClickListener {
+            GoToProfile()
+        }
+        btCancel.setOnClickListener {
+            GoToProfile()
+        }
+    }
+    fun GoToProfile(){
+        val intent = Intent(this, ProfileViewActivity::class.java)
+        startActivity(intent)
     }
 }
