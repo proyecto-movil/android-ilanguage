ea97cde8d85bdf3cd638868f0c73099df1667f46
Josue Huerta <gjhuerta_20@hotmail.com>
2021-10-21 16:42:23 -0500

    Avance

--- app/src/main/java/com/example/ilanguage/MainActivity.kt
+++ app/src/main/java/com/example/ilanguage/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.ilanguage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.prolificinteractive.materialcalendarview.CalendarDay
+import com.prolificinteractive.materialcalendarview.CalendarMode
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        MaterialCalendarView(materialCalendarView)= (MaterialCalendarView) findViewById(R.id.calendarView);
+
+        materialCalendarView.state().edit()
+            .setFirstDayOfWeek(Calendar.MONDAY)
+            .setMinimumDate(CalendarDay.from(2016, 1, 1))
+            .setMaximumDate(CalendarDay.from(2100, 12, 31))
+            .setCalendarDisplayMode(CalendarMode.MONTHS)
+            .commit();
+
+        materialCalendarView.setOnDateChangedListener(new OnDateSelectedListener() {
+            public void onDateSelected
+
+        }
+
+
     }
+
 }
